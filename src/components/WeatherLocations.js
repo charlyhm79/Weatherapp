e58f3b2684b74e3265ab73a709c5462d6a4001c2
Charlyhm79 <charly_hm_@hotmail.com>
2018-04-22 16:00:50 +0200

    Instalamos weathericons
    Instalamos weathericons

---
+++ src/components/WeatherLocations.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Location from './Location';
+
+import WeatherData from "./WeatherData";
+
+
+
+
+const WeatherLocation = () => (
+  <div>
+    <Location city ={'!! Madrid !!'}/>
+    <WeatherData />
+  </div>
+);
+
+
+export default WeatherLocation;
---
+++ src/components/WeatherTemperature.js
@@ -0,0 +1,12 @@
+
+
+import React from "react";
+
+const WeatherTemperature = () => (
+
+<div><span>12 Cº</span></div>
+
+);
+
+export default WeatherTemperature;
+
